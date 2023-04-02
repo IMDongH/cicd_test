bb3cfad95c51ba305661d36c5448b1ca8a70a40f
임동혁 <idh1007@naver.com>
2023-04-02 20:30:04 +0900

    [init]

--- src/main/java/ec2Practice/RequestTestController.java
+++ src/main/java/ec2Practice/RequestTestController.java
@@ -3,15 +3,14 @@ package ec2Practice;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@Controller
+@RestController
 @NoArgsConstructor
-@AllArgsConstructor
-public class TestController {
+public class RequestTestController {
 
     @GetMapping(value = "/test")
     public ResponseEntity getResponse(){
